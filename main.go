4ef78f4a1490c44d5bca554d1b23e36ea00cf381
HHHAAAANNNNN <magmafarhan74@Gmail.com>
2026-02-10 16:36:37 +0700

    initial structures

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("=================================")
+    fmt.Println("  🛒 GO-COMMERCE BACKEND API")
+    fmt.Println("  Selamat Datang di Golang!")
+    fmt.Println("=================================")
+    
+    fmt.Println("\n✅ Setup berhasil!")
+    fmt.Println("📚 Mari belajar Golang bersama!")
+}
